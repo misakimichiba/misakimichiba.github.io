b9b9d4bcfeef3632ffb32dd1b92629206d2973b6
Misaki Michiba <61775829+misakimichiba@users.noreply.github.com>
2024-12-26 16:24:07 +0800

    temp disable

--- main.js
+++ main.js
@@ -132,17 +132,17 @@ tltext.to("#text-animation", {
   ease: "none",
 });
 
-//lenis smooth scroll
-const lenis = new Lenis();
+// //lenis smooth scroll
+// const lenis = new Lenis();
 
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
+// lenis.on("scroll", (e) => {
+//   console.log(e);
+// });
 
-lenis.on("scroll", ScrollTrigger.update);
+// lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
+// gsap.ticker.add((time) => {
+//   lenis.raf(time * 1000);
+// });
 
-gsap.ticker.lagSmoothing(0);
+// gsap.ticker.lagSmoothing(0);
