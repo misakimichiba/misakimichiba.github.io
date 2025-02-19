78d5e398529001a005e4e16dc0876ff71e7a8892
mmichiba <misakimichiba@gmail.com>
2025-02-19 10:27:05 +0800

    Disabled lenis in attempt to fix scroll on mobile

--- main.js
+++ main.js
@@ -141,16 +141,16 @@ tltext.to("#text-animation", {
 });
 
 //lenis smooth scroll
-const lenis = new Lenis();
+// const lenis = new Lenis();
 
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
+// lenis.on("scroll", (e) => {
+//   console.log(e);
+// });
 
-lenis.on("scroll", ScrollTrigger.update);
+// lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
+// gsap.ticker.add((time) => {
+//   lenis.raf(time * 1000);
+// });
 
-gsap.ticker.lagSmoothing(0);
+// gsap.ticker.lagSmoothing(0);
